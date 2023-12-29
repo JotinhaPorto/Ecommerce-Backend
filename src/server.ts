c8e62db2e8a4b1beac5344f68ab8ec881fbf6189
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2023-12-29 15:34:03 -0300

    Initial config to start...  schema was created

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import Express from "express";
+import cors from "cors";
+
+const app = Express();
+
+app.use(cors());
+app.use(Express.json());
+app.use(Express.urlencoded({ extended: true }));
+  
+app.listen(80, () => console.log("Server running on port 80"));
