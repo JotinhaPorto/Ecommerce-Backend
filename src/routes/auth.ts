b9fc3b96210e15148cde47e6cd0388cbba1cef2c
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2024-01-09 17:50:50 -0300

    Using my function profile (return my logged user) in auth routes

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,12 +1,11 @@
 import { Router } from "express";
 import * as Auth from '../controllers/auth'
-import { privateRoute } from "../middleware/passport";
 
 const router = Router()
 
 router.post('/register', Auth.Register)
 router.post('/login', Auth.Login)
-
+router.get('/profile', Auth.Profile)
 
 
 export default router
