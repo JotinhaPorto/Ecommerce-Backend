7a6c3505a2a23a994ffc251322e224d07fa9f3d4
João Pedro Braga Porto <joao.braga.porto01@gmail.com>
2024-01-09 17:48:50 -0300

    change  findUnique to findFirst in getUserbyEmail and id... created func verifyToken

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -7,8 +7,10 @@ type createUserData = Prisma.Args<typeof prisma.user, 'create'>['data']
 
 export const createUser = async (data: createUserData) => await prisma.user.create({ data })
 
-export const getUserByEmail = async (email: string) =>  await prisma.user.findUnique({ where: { email } })
+export const getUserByEmail = async (email: string) => await prisma.user.findFirst({ where: { email } })
 
-export const getUserById = async (id: string) =>  await prisma.user.findUnique({ where: { id } })
+export const getUserById = async (id: string) => await prisma.user.findFirst({ where: { id } })
 
 export const createToken = async (userId: string) => jwt.sign(userId, process.env.JWT_SECRET as string)
+
+export const verifyToken = async (token: string) => jwt.verify(token, process.env.JWT_SECRET as string)
